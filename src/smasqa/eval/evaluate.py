e9016206e0bf43f9b0a456598462ea75edff7e10
iwan-tee <itunyov@gmail.com>
2025-02-23 18:11:01 +0100

    Actualize evaluation process

--- src/smasqa/eval/evaluate.py
+++ src/smasqa/eval/evaluate.py
@@ -5,43 +5,13 @@ import sqlite3
 LOG_FILE = "stat_data_results.csv"  # File to log evaluation results
 MAX_RETRIES = 3  # Number of retries if Swarm fails
 
-
-def get_db_description(db_name):
-    conn = sqlite3.connect(db_name)
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    tables = cursor.fetchall()
-
-    if not tables:
-        return f"No tables found in {db_name}"
-
-    db_description = []
-
-    for table in tables:
-        table_name = table[0]
-
-        cursor.execute(f"PRAGMA table_info({table_name});")
-        columns = cursor.fetchall()
-        column_names = [col[1] for col in columns]
-
-        table_desc = f"Table: {table_name}, Fields: {', '.join(column_names)}"
-        db_description.append(table_desc)
-
-    conn.close()
-
-    return " | ".join(db_description)
-
-def model_run(task, options, db_name="amazon.db",
-              db_description='Table: amazon_sales, Fields: invoice_id, branch, city, customer_type, gender, product_line, unit_price, quantity, vat, total, date, time, payment_method, cogs, gross_margin_percentage, gross_income, rating, time_of_day, day_name, month_name'):
+def model_run(task, options, db_name="amazon.db"):
     """
     Runs Orchestrator with a given task and answer options.
     """
     orchestrator = Orchestrator(
-        task=f"""Your task is: {task}
-        Additional info that you'll need: 
-        Database header row(description): {db_description}
-        Database name: {db_name}""",
+        task=f"Your task is: {task}",
+        database=db_name,
         options=options
     )
     return orchestrator.run()
@@ -59,13 +29,12 @@ def evaluate_row(row):
                f"Answer 4: {row['Answer 4']}"
                ]
     db_name = row['file_name'].replace("csv", "db")
-    db_description = get_db_description(db_name)
 
     result = None
     start_time = time()
     for attempt in range(MAX_RETRIES):
         try:
-            result = model_run(task, options, db_name=db_name, db_description=db_description)
+            result = model_run(task, options, db_name=db_name)
             break  # Exit the loop if successful
         except Exception as e:
             print(f"Swarm error (attempt {attempt + 1}): {e}")
